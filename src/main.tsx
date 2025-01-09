3f62c29016a03d4a15c0144dc508649f6dcda2ae
Franco Garcia <francoexequiel.garcia150@gmail.com>
2025-01-09 16:31:17 -0300

    Updates workflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+if (typeof globalThis.structuredClone !== "function") {
+  globalThis.structuredClone = function <T>(value: T): T {
+    return JSON.parse(JSON.stringify(value));
+  };
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
